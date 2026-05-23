648ed5d113ad1b054401a5a83b7c75ca5c6b0e1c
Arvi_Saleque <alifsalek.as@gmail.com>
2026-05-23 12:55:56 +0600

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,6 @@ import Navbar from "@/components/layout/Navbar";
 import Preloader from "@/components/ui/Preloader";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import GoogleAnalytics from "@/components/analytics/GoogleAnalytics";
 import WhatsAppButton from "@/components/ui/WhatsAppButton";
 import {
   GoogleTagManagerScript,
@@ -69,7 +68,6 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         <GoogleTagManagerNoscript />
-        <GoogleAnalytics />
         <Preloader />
         <Navbar />
         {children}
