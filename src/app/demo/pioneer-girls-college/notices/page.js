ad609b58f1435d98fb28821e610a6d4fda8f21b0
Arvi-saleque <alifsalek.as@gmail.com>
2026-06-29 11:07:18 +0600

    feat(pioneer-college): add coming soon subroutes

--- src/app/demo/pioneer-girls-college/notices/page.js
+++ src/app/demo/pioneer-girls-college/notices/page.js
@@ -1,17 +1,7 @@
-import NoticeBoard from "../components/NoticeBoard";
-import PageHero from "../components/PageHero";
-import SiteShell from "../components/SiteShell";
+import ComingSoonPage, { comingSoonMetadata } from "../components/ComingSoonPage";
+
+export const metadata = comingSoonMetadata;
 
 export default function NoticesPage() {
-  return (
-    <SiteShell>
-      <PageHero
-        eyebrow="নোটিশ"
-        title="নোটিশ বোর্ড ও ঘোষণাসমূহ"
-        description="কলেজের সাম্প্রতিক নোটিশ, পরীক্ষার নির্দেশনা, ফলাফল, বৃত্তি ও অনুষ্ঠানসংক্রান্ত ঘোষণা।"
-        breadcrumbs={[{ label: "নোটিশ" }]}
-      />
-      <NoticeBoard />
-    </SiteShell>
-  );
+  return <ComingSoonPage />;
 }
--- src/app/demo/pioneer-girls-college/teachers/page.js
+++ src/app/demo/pioneer-girls-college/teachers/page.js
@@ -1,18 +1,7 @@
-import PageHero from "../components/PageHero";
-import SiteShell from "../components/SiteShell";
-import TeacherDirectory from "../components/TeacherDirectory";
-import { teachers } from "../data/college-data";
+import ComingSoonPage, { comingSoonMetadata } from "../components/ComingSoonPage";
+
+export const metadata = comingSoonMetadata;
 
 export default function TeachersPage() {
-  return (
-    <SiteShell>
-      <PageHero
-        eyebrow="শিক্ষক ও কর্মচারী"
-        title="শিক্ষকবৃন্দ"
-        description="কলেজের শিক্ষকবৃন্দের নাম, পদবি ও বিভাগভিত্তিক ডিরেক্টরি। ব্যক্তিগত ফোন বা ই-মেইল প্রকাশ করা হয়নি।"
-        breadcrumbs={[{ label: "শিক্ষক ও কর্মচারী" }]}
-      />
-      <TeacherDirectory teachers={teachers} />
-    </SiteShell>
-  );
+  return <ComingSoonPage />;
 }
