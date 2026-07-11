25836d3255d67e48818205aa70af8303a777c08d
Arvi-saleque <alifsalek.as@gmail.com>
2026-07-11 23:52:31 +0600

    refactor: remove custom fonts, preloader, and configure metadataBase

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,59 +1,16 @@
-import { Inter } from "next/font/google";
-import localFont from "next/font/local";
 import "./globals.css";
 import siteConfig from "@/theme/siteConfig";
-
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-sans",
-});
-
-const banglaborno = localFont({
-  src: [
-    {
-      path: "../../public/fonts/Li Alinur Banglaborno Unicode.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Li Alinur Banglaborno Unicode Italic.ttf",
-      weight: "400",
-      style: "italic",
-    },
-  ],
-  display: "swap",
-  variable: "--font-body",
-});
-
-const nakhatra = localFont({
-  src: [
-    {
-      path: "../../public/fonts/Li Alinur Nakkhatra Unicode.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/Li Alinur Nakkhatra Unicode Italic.ttf",
-      weight: "400",
-      style: "italic",
-    },
-  ],
-  display: "swap",
-  variable: "--font-heading",
-});
 
 export const metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
-  icons: {
-    shortcut: "/favicon.ico",
-  },
+  metadataBase: new URL(siteConfig.url),
+  icons: { shortcut: "/favicon.ico" },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${inter.variable} ${banglaborno.variable} ${nakhatra.variable}`}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
